a7c924f9704636f56f2239aa7a63f7f1cd1766d5
MatiZampe <matias.zampelunghe@tonic3.com>
2024-09-23 11:08:11 -0300

    Installing the last version of the packages and adding modelCreating in applicationDbContext

--- template_net_9/ApplicationDbContext.cs
+++ template_net_9/ApplicationDbContext.cs
@@ -10,6 +10,10 @@ namespace template_net_9
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Product> Products { get; set; }
